aee2b4e486996389f462dc826408817b45b469a0
Ayushi Dhiman <160146245+AyushiDhiman76@users.noreply.github.com>
2024-09-19 20:29:42 +0530

    Create Positive or negative.cpp

---
+++ Positive or negative.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter a number:";
+    cin>>num;
+
+    if (num>=0)
+    cout<< num<<"is positive";
+    else
+    cout<< num<<"is negative";
+}
