90612e4807d53e70c4dfef9b9a8242776d95c5aa
Ayushi Dhiman <160146245+AyushiDhiman76@users.noreply.github.com>
2024-09-19 20:35:29 +0530

    Create Check vowels or constants.cpp

---
+++ Check vowels or constants.cpp
@@ -0,0 +1,12 @@
+include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cout<<"Enter character";
+    cin>>c;
+
+    if(c=='a'|| c=='e'|| c=='i'|| c=='o'|| c=='u')
+    cout<<c<<"is vowels";
+    else
+    cout<<c<<"is constants";    
+}
