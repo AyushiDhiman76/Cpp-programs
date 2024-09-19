e99910d45a509ad6bbe531fc973e64514e911c40
Ayushi Dhiman <160146245+AyushiDhiman76@users.noreply.github.com>
2024-09-19 20:28:32 +0530

    Create Pattern 123.cpp

---
+++ Pattern 123.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(i==j)
+            {
+            cout<<i<<" ";
+            }
+            else if(i+j==6)
+            {
+                cout<<i<<"";
+            }
+            else{
+                cout<<" ";
+                }
+        }
+       
+    cout<<endl;
+    }
+return 0;
+}
