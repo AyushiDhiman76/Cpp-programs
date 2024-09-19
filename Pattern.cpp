503a02f27eb9329c01a42ae682c77c5917edc34c
Ayushi Dhiman <160146245+AyushiDhiman76@users.noreply.github.com>
2024-09-19 20:36:39 +0530

    Create Pattern.cpp

---
+++ Pattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows=3;
+
+    for(int i=1;i<=rows;++i){
+        for (int j=1;j<=3;++j){
+            cout<< j <<" ";
+    }
+    cout<< endl;
+    }
+
+}
